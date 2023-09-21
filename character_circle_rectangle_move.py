a671429d228254e38673141eefcd76c4a730eb05
ymyoon(OMEN) <ozone753@naver.com>
2023-09-21 10:42:58 +0900

    루프 한 사이클 완성

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -27,23 +27,20 @@ def run_circle():
 def run_rectangle():
     print('RECTANGLE')
 
-#    for x in range(50,750+1,10):
-#        render_frame(x,90)
-
-#    for x in range(750,50-1,-10):
-#        render_frame(x,550)
+    for x in range(50,750+1,10):
+       render_frame(x,90)
 
     for y in range(90,550+1,10):
          render_frame(750,y)
+
+    for x in range(750,50-1,-10):
+       render_frame(x,550)
          
     for y in range(550,90-1,-10):
          render_frame(50,y)
         
-    pass
-    
-
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
     #testreadtime을 줄여야 한다.
     break
